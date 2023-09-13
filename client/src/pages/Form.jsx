21de8c55ea6b73ec8342659e5e8f798e020a0547
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-09-13 15:43:31 +0700

    feat: set event form

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react"
 
 export default function Form() {
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
+    const [state, setState] = useState({
+        title: '',
+        content: '',
+        author: ''
+    })
+    const {title, content, author} = state
+    
+    const inputValue = name => event => {
+        const value = event.target.value
+        setState({...state, [name]: value})
+    }
 
     return (
         <div className="container pt-5">
@@ -11,15 +19,15 @@ export default function Form() {
             <form className="form-group mt-5">
                 <div>
                     <label>ชื่อบทความ</label>
-                    <input type="text" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                    <input type="text" className="form-control" value={title} onChange={inputValue('title')} />
                 </div>
                 <div className="mt-3">
                     <label>รายละเอียดบทความ</label>
-                    <textarea className="form-control" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                    <textarea className="form-control" value={content} onChange={inputValue('content')}></textarea>
                 </div>
                 <div className="mt-3">
                     <label>ชื่อผู้แต่ง</label>
-                    <input type="text" className="form-control" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    <input type="text" className="form-control" value={author} onChange={inputValue('author')} />
                 </div>
                 <input type="submit" value="Record" className="btn btn-primary mt-3" />
             </form>
