1601f241d937857a535d99001677d1e8adff1287
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-09-13 14:29:08 +0700

    feat: validate blog

--- server/controller/blog.js
+++ server/controller/blog.js
@@ -3,7 +3,16 @@ const slugify = require('slugify')
 exports.create = (req, res) => {
     const {title, content, author} = req.body
     const slug = slugify(title);
-    res.json({
-        data: {title, content, author, slug},
-    })
+
+    switch (true) {
+        case !title:
+            return res.status(400).json({error: 'ไม่มีชื่อบทความ'})
+            break;
+        case !content:
+            return res.status(400).json({error: 'ไม่มีเนิ้อหาบทความ'})
+            break;
+    }
+    // res.json({
+    //     data: {title, content, author, slug},
+    // })
 }
