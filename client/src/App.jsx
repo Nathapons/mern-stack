e9e6e16750345d091bbb83d28a3b29c5843e4be3
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-09-18 16:39:25 +0700

    feat: login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Main from "./pages/Main"
 import Form from "./pages/Form"
 import './App.css'
+import Login from "./pages/Login"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Main />} />
+        <Route path="/login" exact element={<Login />} />
         <Route path="/form/" exact element={<Form />} />
         <Route path="/blog/:id" exact element={<Form />} />
       </Routes>
