c9663bbead3c70b321550e889204947c56e3cd7a
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-09-19 11:50:09 +0700

    feat: logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,29 @@
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Navbar() {
+    const navigate = useNavigate()
+
+    function logout() {
+        sessionStorage.removeItem('token')
+        navigate('/')
+    }
+
     return (
         <nav class="navbar navbar-expand-lg bg-primary text-light">
             <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link text-light" href="/main/">รายละเอียดบทความ</a>
+                        <Link to='/main/' className="nav-link text-light" style={{textDecoration: "none"}}>รายละเอียดบทความ</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-light" href="/form/">เขียนบทความ</a>
+                        <Link to='/form/' className="nav-link text-light" style={{textDecoration: "none"}}>เขียนบทความ</Link>
+                    </li>
+                </ul>
+            </div>
+            <div style={{float: "right"}}>
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item">
+                        <button className="nav-link text-light" onClick={() => logout()}>ออกจากระบบ</button>
                     </li>
                 </ul>
             </div>
