6a7af60f93f47d67d0887b062866b560bbd788a8
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-09-19 11:05:11 +0700

    feat: login component axios

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import axios from 'axios'
 import Swal from "sweetalert2"
-import { authenticate } from "../../service/authorize"
 import { useNavigate } from "react-router-dom"
 
 function LoginComponent(props) {
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import LoginComponent from "../components/LoginComponent";
 import '../style/Login.css'
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+    const navigate = useNavigate();
     useEffect(() => {
         const token = sessionStorage.getItem('token')
         console.log(token)
